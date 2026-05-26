686fc8798a576b9689fbc42f12d70fa5f3462a29
VegTea <1132429901@qq.com>
2026-05-26 13:36:10 +0000

    修正训练脚本

--- start_train.sh
+++ start_train.sh
@@ -2,7 +2,6 @@ python scripts/rsl_rl/train.py \
     --task Snake-VelocityTracking-Flat-v0 \
     --num_envs 4096 \
     --headless \
-    --video \
-    --video_interval 3000 \
-    --video_length 200 \
-    agent.experiment_name=Snake-VelocityTracking-Flat-v0-video-test
+    --run_name exp1 \
+    --max_iterations 15000 \
+    agent.save_interval=1000
