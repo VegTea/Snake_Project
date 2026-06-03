9d9d3511687620de88a40605fb7e77898340cd8a
VegTea <1132429901@qq.com>
2026-06-03 16:31:40 +0800

    exp14

--- start_train.sh
+++ start_train.sh
@@ -2,7 +2,7 @@ python scripts/rsl_rl/train.py \
     --task Snake-VelocityTracking-Flat-v0 \
     --num_envs 4096 \
     --headless \
-    --run_name exp13 \
+    --run_name exp14 \
     --max_iterations 10000 \
     --logger wandb \
     --log_project_name Snake_Project \
